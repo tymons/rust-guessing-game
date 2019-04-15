c19befc05f55d4928cdff5b7355cd1b4cf4a749f
Tymoteusz Cejrowski <tymoteusz.cejrowski@gmail.com>
2019-04-15 21:48:28 +0200

    Added 'random' crate + finishing code.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,34 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
-	println!("Guess the number!");
+
+	let secret_number = rand::thread_rng().gen_range(1, 101);		
+	
 	println!("Please input your guess.");
+	// UTF-8 encoded string	
+	loop {
+		let mut guess = String::new();
 	
-	// UTF-8 encoded string
-	let mut guess = String::new();
-	
-	io::stdin().read_line(&mut guess)
-		.expect("Failed to read line");
-	
-	println!(" You've guessed: {}", guess);
-	
+		io::stdin().read_line(&mut guess)
+			.expect("Failed to read line");
+			
+		let guess: u32 = match guess.trim().parse() {
+			Ok(bytes) => bytes,
+			Err(_)	=> { 
+				println!("Cannot parse. Please enter correct numbers.");
+				continue;
+			}
+		};
+		
+		match guess.cmp(&secret_number) {
+			Ordering::Less => println!("Too small!"),
+			Ordering::Greater => println!("Too big!"),
+			Ordering::Equal => {
+				println!("Yes, you win!");
+				break;
+			}
+		}
+	}	
 }
